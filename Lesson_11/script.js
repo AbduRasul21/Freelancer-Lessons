0852611a9a2b47440a388a6e408bcb2d1af1b4eb
AbduRasul21 <al.rasul.0404@gmail.com>
2021-12-02 16:27:40 +0500

    if switch

--- Lesson_11/script.js
+++ Lesson_11/script.js
@@ -1,10 +1,10 @@
 let age = parseInt(prompt());
 
 
-if (age < 18) {
+if (age > 18) {
     alert("Your age older enough ");
     
 } else {
-    alert("Your age older not enough);
+    alert("Your age older not enough");
 
 }
