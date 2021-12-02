8c04a47a216d3892d97a64435c89d337192f735f
AbduRasul21 <al.rasul.0404@gmail.com>
2021-12-02 13:06:11 +0500

    ended Lesson_10

--- Lesson_10/script.js
+++ Lesson_10/script.js
@@ -4,6 +4,21 @@ let fLen = fruits.length;
 let text = "<ul>";
 const d=new Date();
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 for (let i = 0; i < fLen; i++) {
   text += "<li>" + fruits[i] + "</li>";
 }
@@ -13,4 +28,14 @@ console.log(text);
 
 document.getElementById("demo").innerHTML = text;
 document.getElementById("demo-2").innerHTML = fruits.join(" ");
-document.getElementById("demo-3").innerHTML = d.toISOString();
+document.getElementById("demo-3").innerHTML = d.getFullYear();
+
+let month = [months[d.getMonth()]];
+
+
+document.getElementById("demo-3").innerHTML = d.getUTCDate();
+
+let randomNum = Math.round(Math.random()*100)
+
+
+document.getElementById("demo-3").innerHTML = randomNum;
