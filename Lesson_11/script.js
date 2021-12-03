d72fc8bd614133a33687b21e03091d49d53e3a2f
AbduRasul21 <al.rasul.0404@gmail.com>
2021-12-03 12:24:25 +0500

    Loop

--- Lesson_11/script.js
+++ Lesson_11/script.js
@@ -1,10 +1,39 @@
-let age = parseInt(prompt());
+// let age = parseInt(prompt());
 
 
-if (age > 18) {
-    alert("Your age older enough ");
+// if (age > 18) {
+//     alert("Your age older enough ");
     
-} else {
-    alert("Your age older not enough");
+// } else {
+//     alert("Your age older not enough");
 
+// }
+
+
+
+
+// var text = " ";
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+// for (let x in cars ) {
+//  text += cars[x] + " ";
+//     }
+
+ 
+
+// document.getElementById("demo").innerHTML = text;
+
+const numbers = [45, 4, 9, 16, 25];
+
+let text = " ";
+
+numbers.forEach(myFunction);
+
+
+function myFunction(itself) {
+    text += itself + " ";
 }
+
+document.getElementById("demo").innerHTML = text;
+
+
+
