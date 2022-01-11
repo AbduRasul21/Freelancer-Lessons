9f35c303a278c01c3b8208b2d80b16b200242ada
AbduRasul21 <al.rasul.0404@gmail.com>
2022-01-11 16:34:06 +0500

    practice #4 done

--- Lesson_22/script.js
+++ Lesson_22/script.js
@@ -1,63 +1,77 @@
 // JavaScript basic [150 exercises with solution]
 
 // ----------------------------------------------------------------#1
-switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
+// switch (new Date().getDay()) {
+//   case 0:
+//     day = "Sunday";
+//     break;
 
-  case 1:
-    day = "Monday";
-    break;
+//   case 1:
+//     day = "Monday";
+//     break;
 
-  case 2:
-    day = "Tuesday";
-    break;
+//   case 2:
+//     day = "Tuesday";
+//     break;
 
-  case 3:
-    day = "Wednesday";
-    break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
 
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-    break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+//     break;
 
-  default:
-    break;
-}
+//   default:
+//     break;
+// }
 
-var currentDay = document.getElementById("currentDay");
-var currentTime = document.getElementById("currentTime");
-const d = new Date();
-const time = d.toLocaleTimeString();
-currentDay.innerHTML = "Today is: " + day;
-currentTime.innerHTML = "Current time is:" + time;
+// var currentDay = document.getElementById("currentDay");
+// var currentTime = document.getElementById("currentTime");
+// const d = new Date();
+// const time = d.toLocaleTimeString();
+// currentDay.innerHTML = "Today is: " + day;
+// currentTime.innerHTML = "Current time is:" + time;
 
 // ----------------------------------------------------------------#2
 
-function print_current_window() {
-  window.print();
-}
+// function print_current_window() {
+//   window.print();
+// }
 
 // ----------------------------------------------------------------#3
 
-const dd = d.getDate();
-const mm = d.getMonth() + 1;
-const yy = d.getFullYear();
+// const d = new Date();
+// const dd = d.getDate();
+// const mm = d.getMonth() + 1;
+// const yy = d.getFullYear();
 
-if (dd < 10) {
-  dd = "0" + dd;
-}
+// // if (dd < 10) {
+// //   dd = "0" + dd;
+// // }
 
-if (mm < 10) {
-  mm = "0" + mm;
-}
+// // if (mm < 10) {
+// //   mm = "0" + mm;
+// // }
 
-console.log(mm + "-" + dd + "-" + yy);
-console.log(mm + "/" + dd + "/" + yy);
+// console.log(mm + "-" + dd + "-" + yy);
+// console.log(mm + "/" + dd + "/" + yy);
+
+// ----------------------------------------------------------------#4
+
+
+// var x = 5;
+// var y = 6;
+// var z = 7;
+// var p= (x+y+z)/2
+// var s = Math.sqrt(p * (p - x) * (p - y) * (p - z));
+
+// console.log(s);
+
+// ----------------------------------------------------------------#5
